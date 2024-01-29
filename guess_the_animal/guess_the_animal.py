f83999b3b03675b7bd361c06517a5334436351b7
SandeshGC <gcsandesh01@gmail.com>
2024-01-29 22:12:44 +0545

    fix: same letter occurrences bug

--- guess_the_animal/guess_the_animal.py
+++ guess_the_animal/guess_the_animal.py
@@ -3,6 +3,8 @@ import os
 
 MAX_NUMBER_OF_TRIES = 4
 
+# TODO: something must be done to the spaces in the animal names
+
 
 def main():
     print("\nWelcome to Guess the Animal game.\n")
@@ -15,8 +17,18 @@ def main():
 
     remaining_chances = MAX_NUMBER_OF_TRIES
     revealed_letters = list("_" * len(animal))
+    remaining_letters = list(animal)
 
     while remaining_chances > 0:
+        # print("remaining letters: " + "".join(remaining_letters) + "\n")
+        # print(
+        #     "remainig chances: "
+        #     + str(remaining_chances)
+        #     + "\n"
+        #     + " ".join(remaining_letters)
+        #     + "\n"
+        # )
+
         print(str(remaining_chances) + " chances remaining")
         print(" ".join(revealed_letters))
 
@@ -24,13 +36,17 @@ def main():
 
         os.system("clear || cls")  # clears the terminal after every input
 
-        index_of_character = animal.lower().find(input_str)
+        index_of_character = "".join(remaining_letters).lower().find(input_str)
 
         if index_of_character < 0:
             remaining_chances -= 1
         else:
+            # TODO: Fix that if the letter exists more than once, it will only replace _ with the letter in revealed_letters
+            # and similarly for remaining_letters
             del revealed_letters[index_of_character]
             revealed_letters.insert(index_of_character, animal[index_of_character])
+            del remaining_letters[index_of_character]
+            remaining_letters.insert(index_of_character, "_")
 
         if "".join(revealed_letters) == animal:
             print("You guessed the animal!")
