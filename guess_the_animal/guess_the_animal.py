1c30ffba529d72c685cac4490f2e84c4b5bb1415
SandeshGC <gcsandesh01@gmail.com>
2024-01-30 09:48:13 +0545

    fix: handle cases
    - handle spaces in animal names
    - handle repeated letter guessed by user
    - handle if input is invalid
    - display message, if user has already guessed the letter
    - create reusable functions to modularize the code

--- guess_the_animal/guess_the_animal.py
+++ guess_the_animal/guess_the_animal.py
@@ -3,8 +3,6 @@ import os
 
 MAX_NUMBER_OF_TRIES = 4
 
-# TODO: something must be done to the spaces in the animal names
-
 
 def main():
     print("\nWelcome to Guess the Animal game.\n")
@@ -13,21 +11,14 @@ def main():
     list_of_animals = animals_file.read().splitlines()
     animals_file.close()
 
-    animal = random.choice(list_of_animals)
+    animal = remove_spaces_from_string(random.choice(list_of_animals))
 
     remaining_chances = MAX_NUMBER_OF_TRIES
     revealed_letters = list("_" * len(animal))
     remaining_letters = list(animal)
 
     while remaining_chances > 0:
-        # print("remaining letters: " + "".join(remaining_letters) + "\n")
-        # print(
-        #     "remainig chances: "
-        #     + str(remaining_chances)
-        #     + "\n"
-        #     + " ".join(remaining_letters)
-        #     + "\n"
-        # )
+        print("remaining letters: " + "".join(remaining_letters) + "\n")
 
         print(str(remaining_chances) + " chances remaining")
         print(" ".join(revealed_letters))
@@ -35,25 +26,45 @@ def main():
         input_str = input().lower()
 
         os.system("clear || cls")  # clears the terminal after every input
+        if len(input_str) == 1 and input_str.isalpha():
+            index_of_character = "".join(remaining_letters).lower().find(input_str)
 
-        index_of_character = "".join(remaining_letters).lower().find(input_str)
+            if index_of_character < 0:
+                if "".join(revealed_letters).find(input_str) >= 0:
+                    print("You already guessed that letter.")
+                else:
+                    remaining_chances -= 1
+            else:
+                while index_of_character >= 0:
+                    del_and_insert(
+                        revealed_letters,
+                        index_of_character,
+                        animal[index_of_character],
+                    )
+                    del_and_insert(remaining_letters, index_of_character, "_")
 
-        if index_of_character < 0:
-            remaining_chances -= 1
+                    index_of_character = (
+                        "".join(remaining_letters).lower().find(input_str)
+                    )
+
+            if "".join(revealed_letters) == animal:
+                print("You guessed the animal!")
+                print(animal)
+                return
         else:
-            # TODO: Fix that if the letter exists more than once, it will only replace _ with the letter in revealed_letters
-            # and similarly for remaining_letters
-            del revealed_letters[index_of_character]
-            revealed_letters.insert(index_of_character, animal[index_of_character])
-            del remaining_letters[index_of_character]
-            remaining_letters.insert(index_of_character, "_")
+            print("Enter a single letter.")
 
-        if "".join(revealed_letters) == animal:
-            print("You guessed the animal!")
-            print(animal)
-            return
     print("You could not guess the animal.")
     print("The animal was: " + animal)
+
+
+def remove_spaces_from_string(string):
+    return string.replace(" ", "")
+
+
+def del_and_insert(list, index, character):
+    del list[index]
+    list.insert(index, character)
 
 
 if __name__ == "__main__":
